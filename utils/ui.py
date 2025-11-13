4a6654938521b5823d94b522984f8faa6ef5a076
ctrl-alt-norse <ericsondomingo000@gmail.com>
2025-11-14 01:00:12 +0800

    readme

--- utils/ui.py
+++ utils/ui.py
@@ -93,7 +93,7 @@ def create_map_ui(size: tuple[int, int], locations: dict[str: list[tuple[int, in
 # === CREATE SCREEN INSTRUCTIONS ===
 def create_instructions(level_info: dict, character_cell: str) -> tuple[str]:
 
-    storylines = storyline(parse_level())
+    #storylines = storyline(parse_level())
 
     # Header
     header = (
@@ -143,7 +143,7 @@ def create_instructions(level_info: dict, character_cell: str) -> tuple[str]:
     
     if level_info['game_end']:
         return header+win_message if level_info["mushroom_collected"] == level_info["mushroom_total"] else header+lose_message
-    return header+storylines+description+default_instructions
+    return header+description+default_instructions
 
 
 
